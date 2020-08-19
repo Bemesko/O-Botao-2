63feedeb863399c858a5b8dd1a8d1597e43cc887
Bernardo Mesko <bemesko@gmail.com>
2020-08-19 17:30:02 -0300

    Botão que dá pra clicar

---
+++ Botao2/Assets/Scripts/BtnInputManager.cs
@@ -0,0 +1,13 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// [[RequireComponent(typeof(BtnBehaviour))]
+// public class BtnInputManager : MonoBehaviour
+// {
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (Input.G;
+//     }
+// }
