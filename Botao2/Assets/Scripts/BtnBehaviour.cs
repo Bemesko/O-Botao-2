b7ba795c8159c7cfccb66a5bb30381a056f85521
Bernardo Mesko <bemesko@gmail.com>
2020-08-19 17:58:54 -0300

    Efeito sonoro quando clica no botão

--- Botao2/Assets/Scripts/BtnBehaviour.cs
+++ Botao2/Assets/Scripts/BtnBehaviour.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class BtnBehaviour : MonoBehaviour
 {
+    private AudioSource _audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _audioSource = GetComponentInParent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void OnButtonClicked(){
-        Debug.Log("Clicado!");
+    public void OnButtonClicked()
+    {
+        Debug.Log("Clicado");
+        _audioSource.PlayOneShot(_audioSource.clip);
     }
 }
